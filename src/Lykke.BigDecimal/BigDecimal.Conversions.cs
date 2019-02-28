f424797a56c5d4b5c9ed208ad6a4a7fa7ce0e952
Yury Glushkov <mail@glushkov.us>
2019-02-28 16:08:33 +0300

    Additional explicit conversions added

--- src/Lykke.BigDecimal/BigDecimal.Conversions.cs
+++ src/Lykke.BigDecimal/BigDecimal.Conversions.cs
@@ -2,6 +2,8 @@ namespace System.Numerics
 {
     public partial struct BigDecimal
     {   
+        #region Operators
+        
         public static implicit operator BigDecimal(BigInteger value)
         {
             return new BigDecimal(value);
@@ -16,10 +18,47 @@ namespace System.Numerics
         {
             return Truncate(value)._significand;
         }
+
+        public static explicit operator byte(BigDecimal value)
+        {
+            return (byte) (BigInteger) value;
+        }
+
+        public static explicit operator sbyte(BigDecimal value)
+        {
+            return (sbyte) (BigInteger) value;
+        }
+
+        public static explicit operator short(BigDecimal value)
+        {
+            return (short) (BigInteger) value;
+        }
+
+        public static explicit operator ushort(BigDecimal value)
+        {
+            return (ushort) (BigInteger) value;
+        }
         
         public static explicit operator int(BigDecimal value)
         {
             return (int) (BigInteger) value;
         }
+        
+        public static explicit operator uint(BigDecimal value)
+        {
+            return (uint) (BigInteger) value;
+        }
+        
+        public static explicit operator long(BigDecimal value)
+        {
+            return (long) (BigInteger) value;
+        }
+        
+        public static explicit operator ulong(BigDecimal value)
+        {
+            return (ulong) (BigInteger) value;
+        }
+        
+        #endregion
     }
 }
